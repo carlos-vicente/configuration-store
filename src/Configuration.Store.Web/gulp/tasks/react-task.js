81789f36bea2255a5ed59eb7e510636c4f85f1b2
Carlos Vicente <carlosvicente200@gmail.com>
2017-11-11 23:34:13 +0000

    Update a bunch of npm/yarn packages

--- src/Configuration.Store.Web/gulp/tasks/react-task.js
+++ src/Configuration.Store.Web/gulp/tasks/react-task.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 
 gulp.task('copyReactDist', function(){
-    gulp.src(['node_modules/react/dist/*.*',
-        'node_modules/react-dom/dist/*.*'])
+    gulp.src(['node_modules/react/umd/*.*',
+        'node_modules/react-dom/umd/*.*'])
         .pipe(gulp.dest('Scripts/lib/react/'));
 });
